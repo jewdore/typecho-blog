7f9a66e0ee4b11bce9ebd08387019eb6f26c5cd5
jewdore <jerry.dong@upltv.com>
2019-09-23 23:50:53 +0800

    htaccess

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 
 git archive HEAD -o /tmp/typecho.zip
 scp /tmp/typecho.zip self:/tmp
-ssh self 'mkdir -p  /www/www.91the.top/release; mv  /www/www.91the.top/release  /www/www.91the.top/release.`date +"%Y%m%d%H%M%S"`; unzip /tmp/typecho.zip -d /www/www.91the.top/release;ln -s  /www/etc/typecho.ini.php /www/www.91the.top/release/config.ini.php'
+ssh self 'mkdir -p  /www/www.91the.top/release; mv  /www/www.91the.top/release  /www/www.91the.top/release.`date +"%Y%m%d%H%M%S"`; unzip /tmp/typecho.zip -d /www/www.91the.top/release;cp  /www/etc/typecho.ini.php /www/www.91the.top/release/config.inc.php'
