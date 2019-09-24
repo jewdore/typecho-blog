12bea2663b22ada6e5d06b8d123b420046c7e914
jewdore <jerry.dong@upltv.com>
2019-09-24 14:06:31 +0800

    zephyr

---
+++ usr/themes/Typecho-theme-zephyr-master/footer.php
@@ -0,0 +1,9 @@
+		<footer class="mck-footer">
+			<p class="mck-theme-info">theme zephyr desiged by <a href="http://www.mochiko.cn" class="mck-theme-info-link" target="_target">mochiko</a></p>
+			<p class="mck-copyright-info">mochiko &copy; since 2018</p>
+		</footer>		
+	</div>
+<script src="https://cdn.bootcss.com/jquery/1.8.0/jquery-1.8.0.js"></script>
+<script src="<?php $this->options->themeUrl('js/all.min.js'); ?>""></script>
+</body>
+</html>
