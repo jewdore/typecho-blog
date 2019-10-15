34a07645280dd7553a7dd0881d541e5dbd87bed4
jewdore <jerry.dong@upltv.com>
2019-10-15 12:39:37 +0800

    添加文章内嵌广告

--- usr/themes/initial/page.php
+++ usr/themes/initial/page.php
@@ -11,6 +11,16 @@
 <div class="post-content">
 <?php $this->content(); ?>
 </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-9395958650601631"
+         data-ad-slot="1515821522"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </article>
 <?php $this->need('comments.php'); ?>
 </div>
