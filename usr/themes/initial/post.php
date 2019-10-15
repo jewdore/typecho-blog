d30695d0e1149b32307ba5121d07537967aad76c
jewdore <jerry.dong@upltv.com>
2019-10-15 12:44:41 +0800

    文章阿内敛

--- usr/themes/initial/post.php
+++ usr/themes/initial/post.php
@@ -17,6 +17,16 @@
 <div class="post-content">
 <?php $this->content(); ?>
 </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-9395958650601631"
+         data-ad-slot="1515821522"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 <?php if ($this->options->WeChat || $this->options->Alipay): ?>
 <p class="rewards">打赏: <?php if ($this->options->WeChat): ?>
 <a><img src="<?php $this->options->WeChat(); ?>" alt="微信收款二维码" />微信</a><?php endif; if ($this->options->WeChat && $this->options->Alipay): ?>, <?php endif; if ($this->options->Alipay): ?>
